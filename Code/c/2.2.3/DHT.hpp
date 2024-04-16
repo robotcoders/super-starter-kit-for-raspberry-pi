5b5b26211cef123732c8576ab1f176faac4b39b7
lafvintech <162661251+lafvintech@users.noreply.github.com>
2024-04-16 14:58:19 +0800

    Add files via upload

---
+++ Code/c/2.2.3/DHT.hpp
@@ -0,0 +1,15 @@
+#ifndef _DHT_H_
+#define _DHT_H_
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define maxTim 85
+
+#define dhtPin 0
+
+void readDht11();
+
+#endif
