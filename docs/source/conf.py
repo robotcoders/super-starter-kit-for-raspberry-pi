5c92608d66f801e334df4fb45f99cd5b3b3931c7
robotcoders <167171955+robotcoders@users.noreply.github.com>
2024-04-17 15:34:22 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sphinx_rtd_theme
+import sphinx-conestack-theme
 import time
 
 project = 'Super Starter Kit'
