98a2545a55384fc4b686178d57f977ddd0a0d054
robotcoders <167171955+robotcoders@users.noreply.github.com>
2024-04-17 15:30:06 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,5 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'conestack'
 html_static_path = ['_static']
