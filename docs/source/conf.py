2adb235fb759b8464fa569bfff7d14d6b2398734
robotcoders <167171955+robotcoders@users.noreply.github.com>
2024-04-17 15:50:36 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 import sphinx_conestack_theme
 import time
 
